e7f8c80960c8bdad1d7836dc4a05bb562f0bf258
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-06-25 15:59:08 +0200

    utils - cors & status codes

---
+++ utils/constants/statusCodes.js
@@ -0,0 +1,23 @@
+const queryError = 503;
+const notFound = 206;
+const badGateway = 501;
+const fieldAlreadyExists = 202;
+const success = 200;
+const invalidKey = 500;
+const missingParameters = 401;
+const bcryptError = 205;
+const wrongPassword = 207;
+const badRequest = 400;
+
+module.exports = {
+    badGateway,
+    notFound,
+    queryError,
+    fieldAlreadyExists,
+    success,
+    invalidKey,
+    bcryptError,
+    wrongPassword,
+    missingParameters,
+    badRequest,
+}
