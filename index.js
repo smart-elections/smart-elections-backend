c6cb2fd89c0279d482681c8131c9462468789593
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-06-25 15:59:23 +0200

    setup middleware

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+
+// SETUP MIDDLEWARE
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const cors = require('./utils/helpers/cors')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors);
+app.use(helmet());
+app.enable('trust proxy');
+
+app.get("/", function (req, res) {
+    res.send("Hello World!")
+})
+app.listen(8000, function () {
+    console.log("Started application on port %d", 8000)
+});
