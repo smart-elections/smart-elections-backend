448438694cb23f1773d809321acd06a122e0c8bd
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-07-02 18:57:20 +0200

    added add citizen functionality

--- routes/citizens.routes.js
+++ routes/citizens.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const citizens = require('../services/citizens.service');
 
 router.get('/', citizens.getCitizens);
+router.post('/add/citizen', citizens.addCitizen);
 
 module.exports = router;
--- services/citizens.service.js
+++ services/citizens.service.js
@@ -18,6 +18,32 @@ const getCitizens = (req, res) => {
             });
 }
 
+const addCitizen = (req, res) => {
+
+    let body = req.body
+
+    if (!body.citizen_ssn || !body.citizen_nationality || !body.citizen_firstname || !body.citizen_lastname)
+        res.status(statusCodes.missingParameters).json({ message: "Missing parameters" });
+    else
+        db.query('SELECT * FROM citizens WHERE citizen_ssn = ? and citizen_nationality = ?;', [body.citizen_ssn,
+        body.citizen_nationality], (err, rows) => {
+            if (err)
+                res.status(statusCodes.queryError).json({ error: err });
+            else
+                if (rows[0])
+                    res.status(statusCodes.fieldAlreadyExists).json({ message: "Citizen already exists" });
+                else
+                    db.query('INSERT INTO citizens SET ?;', [body], (err, rows) => {
+                        if (err)
+                            res.status(statusCodes.queryError).json({ error: err });
+                        else
+                            res.status(statusCodes.success).json({ message: "Citizen added successfully" });
+                    });
+        });
+
+}
+
 module.exports = {
-    getCitizens
+    getCitizens,
+    addCitizen
 }
