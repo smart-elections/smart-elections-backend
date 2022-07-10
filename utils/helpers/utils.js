aa91358afc352592639566ef74171b0af940538e
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-07-11 00:52:49 +0200

    added election services

--- utils/helpers/utils.js
+++ utils/helpers/utils.js
@@ -17,6 +17,35 @@ const accountsUpdateCheck = (req) => {
     return { sql, params }
 }
 
+const electionsFetchingCheck = (req) => {
+    let sql = `SELECT election_id, election_year, election_type, election_round, election_start,
+    election_end FROM elections `
+    let params = []
+
+
+    if (req.year || req.round || req.type) {
+        sql += 'WHERE TRUE'
+
+        if (req.year !== undefined) {
+            sql += ` AND election_year = ?`
+            params.push(req.year);
+        }
+        if (req.round !== undefined) {
+            sql += ` AND election_round = ?`
+            params.push(req.round);
+        }
+        if (req.type !== undefined) {
+            sql += ` AND election_type = ?`
+            params.push(req.type);
+        }
+    }
+
+    sql += ';'
+
+    return { sql, params }
+}
+
 module.exports = {
-    accountsUpdateCheck
+    accountsUpdateCheck,
+    electionsFetchingCheck
 }
