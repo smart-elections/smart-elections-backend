226bdd75d64208d14e6475da158a1aa347884c14
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-07-13 17:08:52 +0200

    changed login to POST

--- routes/accounts.routes.js
+++ routes/accounts.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const accounts = require('../services/accounts.service');
 
-router.get('/login', accounts.login);
+router.post('/login', accounts.login);
 router.put('/signup', accounts.signup);
 router.put('/add/wallet', accounts.addWallet);
 router.put('/edit/account', accounts.updateAccount);
