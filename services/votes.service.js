1dd7b0c1b0779a487949e6ae96feef3dbccbda00
Elias Afara <afaraelias@gmail.com>
2022-07-24 11:05:05 +0200

    Fixed typo in checkVotesFetching

--- services/votes.service.js
+++ services/votes.service.js
@@ -1,106 +1,147 @@
 const db = require('../database');
 const statusCodes = require('../utils/constants/statusCodes');
-const { votesFetchingCheck, checkCitizenInVote, checkElectionInVote } = require('../utils/helpers/utils');
+const {
+  checkVotesFetching,
+  checkCitizenInVote,
+  checkElectionInVote,
+} = require('../utils/helpers/utils');
 
 const getVotes = (req, res) => {
+  let { sql, params } = checkVotesFetching(req.query);
 
-    let { sql, params } = votesFetchingCheck(req.query);
-
-    db.query(sql, params, (err, rows) => {
-        if (err) res.status(statusCodes.queryError).json({ error: err });
-        else res.status(statusCodes.success).json({ data: rows });
-    });
-}
+  db.query(sql, params, (err, rows) => {
+    if (err) res.status(statusCodes.queryError).json({ error: err });
+    else res.status(statusCodes.success).json({ data: rows });
+  });
+};
 
 const addVotes = (req, res) => {
-    let body = req.body
+  let body = req.body;
 
-    if (!body.election_year || !body.election_type || !body.election_round ||
-        !body.citizen_ssn || !body.citizen_nationality) {
-        res.status(statusCodes.missingParameters).json({ message: "Missing parameters" });
-    }
-    else {
-        db.query(`SELECT election_id FROM elections WHERE election_year = ? AND election_round = ? 
-        AND election_type = ?;`, [body.election_year, body.election_round, body.election_type],
-            (err, rows) => {
-                if (err) res.status(statusCodes.queryError).json({ error: err });
+  if (
+    !body.election_year ||
+    !body.election_type ||
+    !body.election_round ||
+    !body.citizen_ssn ||
+    !body.citizen_nationality
+  ) {
+    res
+      .status(statusCodes.missingParameters)
+      .json({ message: 'Missing parameters' });
+  } else {
+    db.query(
+      `SELECT election_id FROM elections WHERE election_year = ? AND election_round = ? 
+        AND election_type = ?;`,
+      [body.election_year, body.election_round, body.election_type],
+      (err, rows) => {
+        if (err) res.status(statusCodes.queryError).json({ error: err });
+        else {
+          if (rows[0]) {
+            db.query(
+              `SELECT citizen_id FROM citizens WHERE citizen_ssn = ? AND citizen_nationality = ?;`,
+              [body.citizen_ssn, body.citizen_nationality],
+              (err, rows) => {
+                if (err)
+                  res.status(statusCodes.queryError).json({ error: err });
                 else {
-                    if (rows[0]) {
-                        db.query(`SELECT citizen_id FROM citizens WHERE citizen_ssn = ? AND citizen_nationality = ?;`,
-                            [body.citizen_ssn, body.citizen_nationality],
-                            (err, rows) => {
-                                if (err) res.status(statusCodes.queryError).json({ error: err });
-                                else {
-                                    if (rows[0]) {
-                                        db.query('INSERT INTO votes SET ?;', body,
-                                            (err, rows) => {
-                                                if (err) res.status(statusCodes.queryError).json({ error: err });
-                                                else res.status(statusCodes.success).json({ rowID: rows.insertId })
-                                            })
-                                    }
-                                    else res.status(statusCodes.notFound).json({ message: "Citizen does not exist" });
-                                }
-                            })
-                    }
-                    else {
-                        res.status(statusCodes.notFound).json({ message: "Election does not exist" })
-                    }
+                  if (rows[0]) {
+                    db.query('INSERT INTO votes SET ?;', body, (err, rows) => {
+                      if (err)
+                        res.status(statusCodes.queryError).json({ error: err });
+                      else
+                        res
+                          .status(statusCodes.success)
+                          .json({ rowID: rows.insertId });
+                    });
+                  } else
+                    res
+                      .status(statusCodes.notFound)
+                      .json({ message: 'Citizen does not exist' });
                 }
-            })
-    }
-}
+              }
+            );
+          } else {
+            res
+              .status(statusCodes.notFound)
+              .json({ message: 'Election does not exist' });
+          }
+        }
+      }
+    );
+  }
+};
 
 const editVote = (req, res) => {
-    let id = req.query.id
-    let body = req.body
+  let id = req.query.id;
+  let body = req.body;
 
-    if (!id) {
-        res.status(statusCodes.missingParameters).json({ message: "Missing parameters" });
-    }
-    else {
-        db.query('SELECT vote_id FROM votes WHERE vote_id = ?;', id,
-            (err, rows) => {
-                if (err) res.status(statusCodes.queryError).json({ error: err });
+  if (!id) {
+    res
+      .status(statusCodes.missingParameters)
+      .json({ message: 'Missing parameters' });
+  } else {
+    db.query(
+      'SELECT vote_id FROM votes WHERE vote_id = ?;',
+      id,
+      (err, rows) => {
+        if (err) res.status(statusCodes.queryError).json({ error: err });
+        else {
+          if (rows[0]) {
+            if (body.citizen_ssn || body.citizen_nationality) {
+              let { sql, params } = checkCitizenInVote(body);
+              db.query(sql, params, (err, rows) => {
+                if (err)
+                  res.status(statusCodes.queryError).json({ error: err });
                 else {
-                    if (rows[0]) {
-                        if (body.citizen_ssn || body.citizen_nationality) {
-                            let { sql, params } = checkCitizenInVote(body)
-                            db.query(sql, params,
-                                (err, rows) => {
-                                    if (err) res.status(statusCodes.queryError).json({ error: err });
-                                    else {
-                                        if (rows[0]) {
-                                            let { sql, params } = checkElectionInVote(body);
-                                            db.query(sql, params,
-                                                (err, rows) => {
-                                                    if (err) res.status(statusCodes.queryError).json({ error: err });
-                                                    else {
-                                                        if (rows[0]) {
-                                                            db.query(`UPDATE votes SET ? WHERE vote_id = ?;`, [body, id],
-                                                                (err, rows) => {
-                                                                    if (err) res.status(statusCodes.queryError).json({ error: err });
-                                                                    else res.status(statusCodes.success).json({ message: "Vote updated successfully" });
-                                                                })
-                                                        }
-                                                        else res.status(statusCodes.notFound).json({ message: "Election does not exist" });
-                                                    }
-                                                })
-                                        }
-                                        else res.status(statusCodes.notFound).json({ message: "Citizen does not exist" });
-                                    }
-                                })
-                        }
-                    }
-                    else {
-                        res.status(statusCodes.notFound).json({ message: "Vote not found" });
-                    }
+                  if (rows[0]) {
+                    let { sql, params } = checkElectionInVote(body);
+                    db.query(sql, params, (err, rows) => {
+                      if (err)
+                        res.status(statusCodes.queryError).json({ error: err });
+                      else {
+                        if (rows[0]) {
+                          db.query(
+                            `UPDATE votes SET ? WHERE vote_id = ?;`,
+                            [body, id],
+                            (err, rows) => {
+                              if (err)
+                                res
+                                  .status(statusCodes.queryError)
+                                  .json({ error: err });
+                              else
+                                res
+                                  .status(statusCodes.success)
+                                  .json({
+                                    message: 'Vote updated successfully',
+                                  });
+                            }
+                          );
+                        } else
+                          res
+                            .status(statusCodes.notFound)
+                            .json({ message: 'Election does not exist' });
+                      }
+                    });
+                  } else
+                    res
+                      .status(statusCodes.notFound)
+                      .json({ message: 'Citizen does not exist' });
                 }
-            })
-    }
-}
+              });
+            }
+          } else {
+            res
+              .status(statusCodes.notFound)
+              .json({ message: 'Vote not found' });
+          }
+        }
+      }
+    );
+  }
+};
 
 module.exports = {
-    getVotes,
-    addVotes,
-    editVote
-}
+  getVotes,
+  addVotes,
+  editVote,
+};
