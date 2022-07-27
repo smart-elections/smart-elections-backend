81301de668430acbc2f316517341edf79ee62880
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-07-27 15:55:27 +0200

    added comparision with previous election winner

--- services/analytics.service.js
+++ services/analytics.service.js
@@ -102,7 +102,51 @@ const getElectionWinner = (req, res) => {
                                 (err, rows) => {
                                     if (err) res.status(statusCodes.queryError).json({ error: err });
                                     else {
-                                        if (rows[0]) res.status(statusCodes.success).json({ data: rows[0] });
+                                        if (rows[0]) {
+                                            let electionWinner = rows[0]
+                                            let previousElection = {
+                                                type: req.query.type,
+                                                round: req.query.round
+                                            }
+
+                                            db.query(`SELECT election_year FROM elections WHERE (election_type = ? AND election_round = ?) ORDER BY election_year ASC;`,
+                                                [req.query.type, req.query.round],
+                                                (err, rows) => {
+                                                    if (err) res.status(statusCodes.queryError).json({ error: err });
+                                                    else {
+                                                        if (rows[0]) {
+                                                            let elections = rows
+
+                                                            let previousElectionYearIndex = elections.findIndex((election) => {
+                                                                if (election.election_year === parseInt(req.query.year))
+                                                                    return true
+                                                                else return false
+                                                            }) - 1
+
+                                                            if (previousElectionYearIndex < 0) res.status(statusCodes.notFound).json({ winner: electionWinner, lastElectionDifference: "No previous elections" });
+
+                                                            else {
+                                                                previousElection.year = elections[previousElectionYearIndex].election_year;
+
+                                                                db.query(`SELECT Count(*) votes, candidate_party, candidate_image, citizen_firstname, citizen_lastname FROM votes AS V 
+                                                                INNER JOIN candidates AS CA on V.candidate_id = CA.candidate_id INNER JOIN citizens AS CI on CA.citizen_ssn = CI.citizen_ssn
+                                                                WHERE (election_year = ? AND election_round = ? AND election_type = ?) 
+                                                                GROUP BY V.candidate_id ORDER BY votes DESC;`, [previousElection.year, round, type],
+                                                                    (err, rows) => {
+                                                                        if (err) res.status(statusCodes.queryError).json({ error: err });
+                                                                        else {
+                                                                            let changePercentage = Math.round((electionWinner.votes * 100 / rows[0].votes - 100) * 100) / 100;
+
+                                                                            res.status(statusCodes.notFound).json({ winner: electionWinner, lastElectionDifference: changePercentage });
+                                                                        }
+                                                                    });
+                                                            }
+                                                        }
+                                                        else res.status(statusCodes.notFound).json({ message: "No previous elections found" });
+                                                    }
+                                                });
+                                            // res.status(statusCodes.success).json({ data: rows[0] });
+                                        }
                                         else res.status(statusCodes.notFound).json({ message: "No winner declared yet" })
                                     }
                                 });
@@ -113,10 +157,6 @@ const getElectionWinner = (req, res) => {
                 }
             })
     }
-}
-
-const getWinnerVotes = (req, res) => {
-
 }
 
 module.exports = {
