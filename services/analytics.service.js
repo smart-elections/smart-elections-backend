a5c26445fc40e7d6796651fee7a9ab4c8c2d900c
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-07-27 15:39:10 +0200

    more bug fixes

--- services/analytics.service.js
+++ services/analytics.service.js
@@ -93,20 +93,23 @@ const getElectionWinner = (req, res) => {
                 if (err) res.status(statusCodes.queryError).json({ error: err });
                 else {
                     let currentDate = new Date();
-                    if (rows[0].election_end.getTime() < currentDate.getTime()) {
-                        db.query(`SELECT Count(*) votes, candidate_party, candidate_image, citizen_firstname, citizen_lastname FROM votes AS V 
+                    if (rows[0]) {
+                        if (rows[0].election_end.getTime() < currentDate.getTime()) {
+                            db.query(`SELECT Count(*) votes, candidate_party, candidate_image, citizen_firstname, citizen_lastname FROM votes AS V 
                         INNER JOIN candidates AS CA on V.candidate_id = CA.candidate_id INNER JOIN citizens AS CI on CA.citizen_ssn = CI.citizen_ssn
                         WHERE (election_year = ? AND election_round = ? AND election_type = ?) 
                         GROUP BY V.candidate_id ORDER BY votes DESC;`, [year, round, type],
-                            (err, rows) => {
-                                if (err) res.status(statusCodes.queryError).json({ error: err });
-                                else {
-                                    if (rows[0]) res.status(statusCodes.success).json({ data: rows[0] });
-                                    else res.status(statusCodes.notFound).json({ message: "No winner declared yet" })
-                                }
-                            })
+                                (err, rows) => {
+                                    if (err) res.status(statusCodes.queryError).json({ error: err });
+                                    else {
+                                        if (rows[0]) res.status(statusCodes.success).json({ data: rows[0] });
+                                        else res.status(statusCodes.notFound).json({ message: "No winner declared yet" })
+                                    }
+                                });
+                        }
+                        else res.status(statusCodes.notFound).json({ message: "Election hasn't ended yet" });
                     }
-                    else res.status(statusCodes.notFound).json({ message: "Election hasn't ended yet" })
+                    else res.status(statusCodes.notFound).json({ message: "Election doesn't exist" });
                 }
             })
     }
