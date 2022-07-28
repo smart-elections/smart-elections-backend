545cb88341b16697488aa28920246357fbc73180
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-07-29 01:09:43 +0200

    fixed add vote bug

--- services/votes.service.js
+++ services/votes.service.js
@@ -23,56 +23,67 @@ const addVotes = (req, res) => {
     res.status(statusCodes.missingParameters).json({ message: 'Missing parameters' });
   }
   else {
-    db.query(`SELECT vote_id FROM votes WHERE election_year = ? AND election_type = ? AND election_round = ? 
-    AND citizen_ssn = ? AND citizen_nationality = ?;`, [body.election_year, body.election_type, body.election_round,
-    body.citizen_ssn, body.citizen_nationality],
+    db.query(`SELECT registered_voter_id FROM registered_voters WHERE (citizen_ssn = ? AND citizen_nationality = ?
+      AND election_year = ? AND election_type = ? AND election_round = ?);`, [body.citizen_ssn, body.citizen_nationality,
+    body.election_year, body.election_type, body.election_round],
       (err, rows) => {
         if (err) res.status(statusCodes.queryError).json({ error: err });
         else {
-          if (rows[0]) res.status(statusCodes.fieldAlreadyExists).json({ message: "Citizen already voted" });
+          if (rows[0]) res.status(statusCodes.notFound).json({ message: "Citizen not registered to vote" });
           else {
-            db.query(`SELECT election_id FROM elections WHERE election_year = ? AND election_round = ? AND election_type = ?;`,
-              [body.election_year, body.election_round, body.election_type],
+            db.query(`SELECT vote_id FROM votes WHERE election_year = ? AND election_type = ? AND election_round = ? 
+            AND citizen_ssn = ? AND citizen_nationality = ?;`, [body.election_year, body.election_type, body.election_round,
+            body.citizen_ssn, body.citizen_nationality],
               (err, rows) => {
                 if (err) res.status(statusCodes.queryError).json({ error: err });
                 else {
-                  if (rows[0]) {
-                    db.query(`SELECT citizen_id FROM citizens WHERE citizen_ssn = ? AND citizen_nationality = ?;`,
-                      [body.citizen_ssn, body.citizen_nationality],
+                  if (rows[0]) res.status(statusCodes.fieldAlreadyExists).json({ message: "Citizen already voted" });
+                  else {
+                    db.query(`SELECT election_id FROM elections WHERE election_year = ? AND election_round = ? AND election_type = ?;`,
+                      [body.election_year, body.election_round, body.election_type],
                       (err, rows) => {
                         if (err) res.status(statusCodes.queryError).json({ error: err });
                         else {
                           if (rows[0]) {
-                            db.query(`SELECT election_year, election_round, election_type FROM election_candidate WHERE candidate_id = ?`, body.candidate_id,
+                            db.query(`SELECT citizen_id FROM citizens WHERE citizen_ssn = ? AND citizen_nationality = ?;`,
+                              [body.citizen_ssn, body.citizen_nationality],
                               (err, rows) => {
                                 if (err) res.status(statusCodes.queryError).json({ error: err });
                                 else {
                                   if (rows[0]) {
-                                    if (rows[0].election_year === body.election_year && rows[0].election_round === body.election_round
-                                      && rows[0].election_type === body.election_type) {
-                                      db.query('INSERT INTO votes SET ?;', body, (err, rows) => {
-                                        if (err)
-                                          res.status(statusCodes.queryError).json({ error: err });
-                                        else
-                                          res.status(statusCodes.success).json({ rowID: rows.insertId });
-                                      });
-                                    }
-                                    else res.status(statusCodes.notFound).json({ message: "Candidate isn't in selected election" });
-                                  }
-                                  else res.status(statusCodes.notFound).json({ message: "Candidate doesn't exist" });
+                                    db.query(`SELECT election_year, election_round, election_type FROM election_candidate WHERE candidate_id = ?`, body.candidate_id,
+                                      (err, rows) => {
+                                        if (err) res.status(statusCodes.queryError).json({ error: err });
+                                        else {
+                                          if (rows[0]) {
+                                            if (rows[0].election_year === body.election_year && rows[0].election_round === body.election_round
+                                              && rows[0].election_type === body.election_type) {
+                                              db.query('INSERT INTO votes SET ?;', body, (err, rows) => {
+                                                if (err)
+                                                  res.status(statusCodes.queryError).json({ error: err });
+                                                else
+                                                  res.status(statusCodes.success).json({ rowID: rows.insertId });
+                                              });
+                                            }
+                                            else res.status(statusCodes.notFound).json({ message: "Candidate isn't in selected election" });
+                                          }
+                                          else res.status(statusCodes.notFound).json({ message: "Candidate doesn't exist" });
+                                        }
+                                      })
+                                  } else
+                                    res.status(statusCodes.notFound).json({ message: 'Citizen does not exist' });
                                 }
-                              })
-                          } else
-                            res.status(statusCodes.notFound).json({ message: 'Citizen does not exist' });
+                              }
+                            );
+                          } else {
+                            res.status(statusCodes.notFound).json({ message: 'Election does not exist' });
+                          }
                         }
                       }
                     );
-                  } else {
-                    res.status(statusCodes.notFound).json({ message: 'Election does not exist' });
                   }
                 }
-              }
-            );
+              });
           }
         }
       });
